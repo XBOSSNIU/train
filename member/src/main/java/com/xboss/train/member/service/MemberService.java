66e754715a9e734993643c70be279dddc0bd7bae
XBOSSNIU <xboss8888@163.com>
2024-03-05 22:36:29 +0800

    集成mybatis官方代码生成器

--- member/src/main/java/com/xboss/train/member/service/MemberService.java
+++ member/src/main/java/com/xboss/train/member/service/MemberService.java
@@ -1,16 +1,36 @@
 package com.xboss.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.xboss.train.member.domain.Member;
+import com.xboss.train.member.domain.MemberExample;
 import com.xboss.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MemberService {
-
     @Resource
     private MemberMapper memberMapper;
 
     public int count(){
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public long register(String mobile){
+        MemberExample memberExample=new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+
+        if(CollUtil.isNotEmpty(list)){
+            throw new RuntimeException("手机号已注册");
+        }
+        Member member=new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
