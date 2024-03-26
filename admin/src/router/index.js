b10798fbd9abe819f95e94486acb43d5ea370378
XBOSSNIU <xboss8888@163.com>
2024-03-26 20:40:24 +0800

    新增每日车厢功能与页面

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -49,14 +49,20 @@ const routes = [
             },
             {
                 path:'business/',
-                children: [{
+                children: [
+                    {
                     path:'daily-train',
                     component:()=>import('../views/main/business/daily-train.vue')
-                },
+                    },
                     {
                         path:'daily-train-station',
                         component:()=>import('../views/main/business/daily-train-station.vue')
-                    }]
+                    },
+                    {
+                        path:'daily-train-carriage',
+                        component:()=>import('../views/main/business/daily-train-carriage.vue')
+                    }
+                ]
             }]
     },
   {
